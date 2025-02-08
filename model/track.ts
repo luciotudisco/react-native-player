acbe73f2e1b92378e55890923c0aca43e803f046
luciotudisco <lucio.tudisco@gmail.com>
2025-02-08 23:06:05 +0000

    PlayList component

--- model/track.ts
+++ model/track.ts
@@ -26,31 +26,31 @@ export const DEMO_PLAYLIST: PlayList = new PlayList('Demo Playlist', [
   new PlayListItem(
     'Frosted Beats',
     'Luna Harmonix',
-    '../assets/audio/track1.mp3',
+    'https://res.cloudinary.com/djcq86lcz/video/upload/v1739055848/track1_kflqao.mp3',
     'https://res.cloudinary.com/djcq86lcz/image/upload/v1739055148/cover1_y0kmq4.webp',
   ),
   new PlayListItem(
     'Snow-Capped Reverie',
     'Vesper Cloud',
-    '../assets/audio/track1.mp3',
+    'https://res.cloudinary.com/djcq86lcz/video/upload/v1739055851/track2_cyjcvj.mp3',
     'https://res.cloudinary.com/djcq86lcz/image/upload/v1739055148/cover2_x4pfrh.webp',
   ),
   new PlayListItem(
     'Midnight Ember Chill',
     'Koji Glacier',
-    '../assets/audio/track1.mp3',
+    'https://res.cloudinary.com/djcq86lcz/video/upload/v1739055848/track3_ylaziu.mp3',
     'https://res.cloudinary.com/djcq86lcz/image/upload/v1739055148/cover3_pxmllh.webp',
   ),
   new PlayListItem(
     'Hushed Winter Echoes',
     'Neon Polaris',
-    '../assets/audio/track1.mp3',
+    'https://res.cloudinary.com/djcq86lcz/video/upload/v1739055849/track4_i3xviu.mp3',
     'https://res.cloudinary.com/djcq86lcz/image/upload/v1739055148/cover4_eomoho.webp',
   ),
   new PlayListItem(
     'Starfall Drizzle',
     'Mellow Pine',
-    '../assets/audio/track1.mp3',
+    'https://res.cloudinary.com/djcq86lcz/video/upload/v1739055848/track5_gdvhth.mp3',
     'https://res.cloudinary.com/djcq86lcz/image/upload/v1739055148/cover5_uiawor.webp',
   ),
 ]);
