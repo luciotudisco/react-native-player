cd50beaa630e7f1412541475c226241f5e01b33b
luciotudisco <lucio.tudisco@gmail.com>
2025-02-10 07:18:14 +0000

    PlayList component

--- store/store.ts
+++ store/store.ts
@@ -35,14 +35,23 @@ const DEFAULT_STATE: AudioPlayerState = {
 
 export const useAudioPlayerStore = create<AudioPlayerState & AudioPlayerActions>((set, get) => ({
   ...DEFAULT_STATE,
+  /**
+   * Returns true if the current track is the first track in the playlist.
+   */
   isFirstTrack: () => {
     const { playlist, currentTrack } = get();
     return playlist.items.findIndex((item) => item.id === currentTrack.item.id) === 0;
   },
+  /**
+   * Returns true if the current track is the last track in the playlist.
+   */
   isLastTrack: () => {
     const { playlist, currentTrack } = get();
     return playlist.items.findIndex((item) => item.id === currentTrack.item.id) === playlist.items.length - 1;
   },
+  /**
+   * Moves to the next track in the playlist.
+   */
   next: () => {
     const { playlist, currentTrack } = get();
     const currentIndex = playlist.items.findIndex((item) => item.id === currentTrack.item.id);
@@ -51,10 +60,16 @@ export const useAudioPlayerStore = create<AudioPlayerState & AudioPlayerActions>
     }
     set({ currentTrack: { ...currentTrack, item: playlist.items[currentIndex + 1] } });
   },
+  /**
+   * Pauses the current track.
+   */
   pause: async () => {
     get().currentTrack.sound?.pauseAsync();
     set((state) => ({ currentTrack: { ...state.currentTrack, isPlaying: false } }));
   },
+  /**
+   * Plays the current track from the beginning.
+   */
   play: async () => {
     const { currentTrack } = get();
     if (currentTrack.sound) {
@@ -65,6 +80,9 @@ export const useAudioPlayerStore = create<AudioPlayerState & AudioPlayerActions>
     sound.playAsync();
     set({ currentTrack: { isPlaying: true, sound, item: currentTrack.item } });
   },
+  /**
+   * Moves to the previous track in the playlist.
+   */
   previous: () => {
     const { playlist, currentTrack } = get();
     const currentIndex = playlist.items.findIndex((item) => item.id === currentTrack.item.id);
@@ -73,6 +91,9 @@ export const useAudioPlayerStore = create<AudioPlayerState & AudioPlayerActions>
     }
     set({ currentTrack: { ...currentTrack, item: playlist.items[currentIndex - 1] } });
   },
+  /**
+   * Resumes the current track from where it was paused.
+   */
   resume: async () => {
     const { currentTrack } = get();
     if (currentTrack.sound) {
@@ -80,5 +101,8 @@ export const useAudioPlayerStore = create<AudioPlayerState & AudioPlayerActions>
       set((state) => ({ currentTrack: { ...state.currentTrack, isPlaying: true } }));
     }
   },
+  /**
+   * Moves to the specified track in the playlist.
+   */
   setTrack: (item: PlayListItem) => set((state) => ({ currentTrack: { ...state.currentTrack, item } })),
 }));
